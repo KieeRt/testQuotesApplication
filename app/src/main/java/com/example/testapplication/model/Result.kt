298f2570dbe1fc34d0bcd59076c816b1a0124b39
dimal <dimalozyak1997@gmail.com>
2022-04-15 16:16:54 +0200

    created listener to passed for adapter

--- app/src/main/java/com/example/testapplication/model/Result.kt
+++ app/src/main/java/com/example/testapplication/model/Result.kt
@@ -21,4 +21,21 @@ data class Result(
         println("tags:${tags.joinToString(separator = "") { "${it.toString()}\n" }} ")
 
     }
+
+    override fun equals(other: Any?): Boolean {
+        if(this === other) return true
+
+        if(other is Result){
+            val tmp : Result = other
+            if(this._id == other._id) return true
+        }
+
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return this._id.toInt()
+    }
+
+
 }
