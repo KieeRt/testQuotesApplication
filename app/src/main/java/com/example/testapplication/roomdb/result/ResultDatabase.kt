457425bf8f4a853fb913916fecfa94188968e8b8
dimal <dimalozyak1997@gmail.com>
2022-04-20 17:34:25 +0200

    add converter to field of entity ( Result )

--- app/src/main/java/com/example/testapplication/roomdb/result/ResultDatabase.kt
+++ app/src/main/java/com/example/testapplication/roomdb/result/ResultDatabase.kt
@@ -5,5 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.testapplication.model.Result
 
 @Database(entities = [Result::class], version = 1)
+
+
 abstract class ResultDatabase : RoomDatabase() {
 }
