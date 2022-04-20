1150ba03d3b7ebcf0c81af76bf1f0f3ebecdd079
dimal <dimalozyak1997@gmail.com>
2022-04-20 17:42:11 +0200

    fix problem with Converters.kt

--- app/src/main/java/com/example/testapplication/roomdb/result/ResultDatabase.kt
+++ app/src/main/java/com/example/testapplication/roomdb/result/ResultDatabase.kt
@@ -1,11 +1,38 @@
 package com.example.testapplication.roomdb.result
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.testapplication.model.Result
 
 @Database(entities = [Result::class], version = 1)
-
-
+@TypeConverters(Converters::class)
 abstract class ResultDatabase : RoomDatabase() {
+    abstract fun resultDao(): ResultDao
+
+    companion object {
+        // Singleton prevents multiple
+        // instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: ResultDatabase? = null
+
+        fun getDatabase(context: Context): ResultDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ResultDatabase::class.java,
+                    "database-name"
+                ).allowMainThreadQueries().addTypeConverter(Converters::class).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+    
 }
