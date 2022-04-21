152306df516a88aae35cb2479bc375d08fc324a2
dimal <dimalozyak1997@gmail.com>
2022-04-21 15:36:57 +0200

    add method for room database

--- app/src/main/java/com/example/testapplication/roomdb/result/ResultDao.kt
+++ app/src/main/java/com/example/testapplication/roomdb/result/ResultDao.kt
@@ -1,5 +1,6 @@
 package com.example.testapplication.roomdb.result
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.testapplication.model.Result
@@ -9,6 +10,20 @@ interface ResultDao {
     @Query("SELECT * FROM result")
     fun getAll(): List<Result>
 
+    @Query("SELECT * FROM result WHERE _id = :id")
+    fun findById(id : Int)
+
+    @Query("SELECT * FROM result WHERE author = :author")
+    fun findByAuthor(author: String) : List<Result>
+
     @Insert
     fun insertAll(result: List<Result>)
+
+    @Insert
+    fun insert(result: Result)
+
+    @Delete
+    fun delete(result: Result)
+
+
 }
